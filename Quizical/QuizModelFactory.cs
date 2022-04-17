23b5135792a38a1d92be89c074d847cdc4132716
Saumya <n01487261@humbermail.ca>
2022-04-16 21:28:53 -0400

    Added singleton, flyweight,factory code

---
+++ Quizical/QuizModelFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quizical
+{
+    //Singleton Factory class
+    internal class QuizModelFactory
+    {
+
+        private static QuizModelFactory? quizModelFactory;
+        private Dictionary<string,QuizModel> quizModelDictionary = new Dictionary<string, QuizModel>();
+        
+
+        private QuizModelFactory() { }
+
+        public static QuizModelFactory GetInstance()
+        {
+            if (quizModelFactory == null)
+            {
+                quizModelFactory = new QuizModelFactory();
+            }
+            return quizModelFactory;
+        }
+
+        internal IQuizModel GetQuizModel(string quizModel)
+        {
+            if (!quizModelDictionary.ContainsKey(quizModel))
+            {
+                quizModelDictionary.Add(quizModel, new QuizModel(quizModel));
+            }
+            return quizModelDictionary[quizModel];
+        }
+
+        //Private Flyweight model class
+        private class QuizModel : IQuizModel
+        {
+            private int score;
+
+            //Model constructor
+            public QuizModel(string quizModel)
+            {
+                score = 0;
+            }
+
+            public void checkAnswer()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void displayScore()
+            {
+                Console.WriteLine("Your score is : "+score);
+            }
+        }
+
+        
+    }
+}
