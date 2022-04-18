fdba8cd762d2e3ef9ceb837d085772685d3d553a
BhupinderSinghVirdi <n01435365@humbermail.ca>
2022-04-17 23:07:57 -0400

    Comments Added

--- Quizical/GenreBiology.cs
+++ Quizical/GenreBiology.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Quizical
 {
-
+    //Prototype class
     abstract class GenrePrototype
     {
         public abstract GenrePrototype Clone();
@@ -18,17 +18,19 @@ namespace Quizical
 
         public GenreBiology() 
         {
-            questionBank.Add(1, new Genre("Question : How many hairs does the average person have on their head?", 2));
-            questionBank.Add(2, new Genre("Question : How many bones make up our whole skeleton?", 1));
-            questionBank.Add(3, new Genre("Question : Which type of blood cells help our bodies to fight infection?", 1));
-            questionBank.Add(4, new Genre("Question : Approximately how many times does your heart beat every 24 hours?", 3));
-            questionBank.Add(5, new Genre("Question : The average adult human brain weighs about ……?", 1));
+            questionBank.Add(1, new Genre("Question : How many hairs does the average person have on their head? 1) Approximately 25,000 \t  2) Approx. 125,000  \t  3) Approx. 1,125,000", 2));
+            questionBank.Add(2, new Genre("Question : How many bones make up our whole skeleton? 1) 206 \t 2) 306 \t 3) 406", 1));
+            questionBank.Add(3, new Genre("Question : Which type of blood cells help our bodies to fight infection? 1) White blood cells \t 2) Red blood cells  \t 3) Both red and white blood cells", 1));
+            questionBank.Add(4, new Genre("Question : Approximately how many times does your heart beat every 24 hours? 1) 1000 times  \t 2) 10,000 times  \t 3) 100,000 times", 3));
+            questionBank.Add(5, new Genre("Question : The average adult human brain weighs about ……? 1) 1.5kg \t 2) 2.5kg \t 3) 3.5kg ", 1));
         }
 
         public override GenrePrototype Clone()
         {
             return this.MemberwiseClone() as GenreBiology;
         }
+
+        //implementing the Interface Methods
 
         public List<int> getAnswers()
         {
--- Quizical/GenreGeology.cs
+++ Quizical/GenreGeology.cs
@@ -11,12 +11,15 @@ namespace Quizical
         public Dictionary<int, Genre> questionBank = new Dictionary<int, Genre>();
         public GenreGeology()
         {
-            questionBank.Add(1, new Genre("Question :In which Italian city can you find the Colosseum?", 4));
-            questionBank.Add(2, new Genre("In which city is the Juventus Football Club based?", 4));
-            questionBank.Add(3, new Genre("Which city was covered in ashes during the eruption of Mount Vesuvius in 79 AD?", 2));
-            questionBank.Add(4, new Genre("In which city was Martin Luther King Jr. assassinated?", 4));
-            questionBank.Add(5, new Genre("In which city were the 2000 Summer Olympics held?", 3));
+            questionBank.Add(1, new Genre("Question :In which Italian city can you find the Colosseum? 1) Venice \t 2) Rome \t 3) \t Naples 4) Milan", 2));
+            questionBank.Add(2, new Genre("In which city is the Juventus Football Club based? 1) Turin \t 2) Barcelona \t 3) Manchester \t 4) Marseille", 1));
+            questionBank.Add(3, new Genre("Which city was covered in ashes during the eruption of Mount Vesuvius in 79 AD? 1) Rome \t 2) Catania \t 3) Pompeii \t 4) Naples", 3));
+            questionBank.Add(4, new Genre("In which city was Martin Luther King Jr. assassinated? 1) New York \t 2)  Austin \t 3) Miami \t  4) Memphis", 4));
+            questionBank.Add(5, new Genre("In which city were the 2000 Summer Olympics held? 1) London \t 2) Paris \t 3) Barcelona \t 4)Sydney", 4));
         }
+
+        //implementing the Interface Methods
+
         public List<int> getAnswers()
         {
             List<int> answers = new List<int>();
@@ -37,7 +40,7 @@ namespace Quizical
             return Questions;
         }
 
-        // Returns dictionary with all the specific genre details and  
+        
         public Dictionary<int, Genre> getQuestionDetails()
         {
             return questionBank;
