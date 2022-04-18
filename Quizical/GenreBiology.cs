3deff68d008fe1927896b60fc1d77b95cdfdbd38
BhupinderSinghVirdi <n01435365@humbermail.ca>
2022-04-17 20:12:17 -0400

    Prototype Classes complete

--- Quizical/GenreBiology.cs
+++ Quizical/GenreBiology.cs
@@ -14,28 +14,46 @@ namespace Quizical
 
     internal class GenreBiology : GenrePrototype, GenreInterface
     {
-        public Dictionary<Genre, int> questionBank = new Dictionary<Genre, int>();
+        public Dictionary<int, Genre> questionBank = new Dictionary<int, Genre>();
 
         public GenreBiology() 
         {
-            
+            questionBank.Add(1, new Genre("Question : How many hairs does the average person have on their head?", 2));
+            questionBank.Add(2, new Genre("Question : How many bones make up our whole skeleton?", 1));
+            questionBank.Add(3, new Genre("Question : Which type of blood cells help our bodies to fight infection?", 1));
+            questionBank.Add(4, new Genre("Question : Approximately how many times does your heart beat every 24 hours?", 3));
+            questionBank.Add(5, new Genre("Question : The average adult human brain weighs about ……?", 1));
         }
 
         public override GenrePrototype Clone()
         {
-
-            Console.WriteLine("blahblahblah");
             return this.MemberwiseClone() as GenreBiology;
         }
 
-        public void getAnswers()
+        public List<int> getAnswers()
         {
-            throw new NotImplementedException();
+            List<int> answers = new List<int>();
+            foreach (Genre question in questionBank.Values)
+            {
+                answers.Add(question.Answer);
+            }
+            return answers;
         }
 
-        public void getQuestions()
+        public List<string> getQuestions()
         {
-            
+            List<string> Questions = new List<string>();
+            foreach (Genre question in questionBank.Values)
+            {
+                Questions.Add(question.Question);
+            }
+            return Questions;
         }
+
+        public Dictionary<int, Genre> getQuestionDetails()
+        {
+            return questionBank;
+        }
+
     }
 }
--- Quizical/GenreTrivia.cs
+++ Quizical/GenreTrivia.cs
@@ -8,9 +8,19 @@ namespace Quizical
 {
     internal class GenreTrivia : GenrePrototype, GenreInterface
     {
+        GenreBiology gB =  new GenreBiology();
+
+        public Dictionary<int, Genre> questionBank = new Dictionary<int, Genre>();
+
+
         public GenreTrivia()
         {
-
+            GenreTrivia gT = (GenreTrivia)gB.Clone();
+            gT.questionBank.Add(1, new Genre("Question : Who was elected President of the United States in 2017?",1));
+            gT.questionBank.Add(2, new Genre("Question : When did Jonas Brothers make their comeback to the music world?", 3));
+            gT.questionBank.Add(3, new Genre("Question : What is the national language of Canada?", 2));
+            gT.questionBank.Add(4, new Genre("Question : A la Crecy is a French dish made of what?", 2));
+            gT.questionBank.Add(5, new Genre("Question : What native country is Brazil?", 2));
         }
 
         public override GenrePrototype Clone()
@@ -18,14 +28,29 @@ namespace Quizical
             return this.MemberwiseClone() as GenreTrivia;
         }
 
-        public void getAnswers()
+        public List<int> getAnswers()
         {
-            throw new NotImplementedException();
+            List<int> answers = new List<int>();
+            foreach (Genre question in questionBank.Values)
+            {
+                answers.Add(question.Answer);
+            }
+            return answers;
         }
 
-        public void getQuestions()
+        public List<string> getQuestions()
         {
-            throw new NotImplementedException();
+            List<string> Questions = new List<string>();
+            foreach (Genre question in questionBank.Values)
+            {
+                Questions.Add(question.Question);
+            }
+            return Questions;
+        }
+
+        public Dictionary<int, Genre> getQuestionDetails()
+        {
+            return questionBank;
         }
     }
 }
